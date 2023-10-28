a233ef4c4ca977caacea0fadf04e6b5624b922a5
Trevor Bonomi <trevorbonomi@gmail.com>
2023-10-28 20:54:51 +0200

    missed it

--- packages/client/src/lib/ApolloClient.ts
+++ packages/client/src/lib/ApolloClient.ts
@@ -3,14 +3,14 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 
-const APOLLO_PORT = process.env.APOLLO_PORT;
-const APOLLO_SERVER_URL = process.env.APOLLO_SERVER_URL;
+const CLIENT_APOLLO_PORT = process.env.CLIENT_APOLLO_PORT;
+const CLIENT_APOLLO_SERVER_URL = process.env.CLIENT_APOLLO_SERVER_URL;
 
 export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-      uri: `${APOLLO_SERVER_URL}:${APOLLO_PORT}/graphql`,
+      uri: `${CLIENT_APOLLO_SERVER_URL}:${CLIENT_APOLLO_PORT}/graphql`,
       // you can disable result caching here if you want to
       // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
       // fetchOptions: { cache: "no-store" },
